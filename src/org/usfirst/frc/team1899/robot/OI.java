58a076675bcd485bc51f968036d91a34e83d1e13
Shreyas <skraman1999@gmail.com>
2015-01-24 14:25:30 -0800

    Finished OI.

--- src/org/usfirst/frc/team1899/robot/OI.java
+++ src/org/usfirst/frc/team1899/robot/OI.java
@@ -1,58 +1,46 @@
 package org.usfirst.frc.team1899.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.RobotDrive;
-
-import org.usfirst.frc.team1899.robot.commands.DriveCommand;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
-	Joystick driveStick = new Joystick(1);
-	Joystick driveStick2 = new Joystick(2);
-	
-	public double getAxisValue(int axis) {
-		if(axis == 1)
-			return driveStick.getX();
-		if(axis == 2)
-			return driveStick.getY();
-		if(axis == 3)
-			return driveStick.getZ();
-		return 0;
-		//return axis;
-	}
-	//drivestick.AxisType();
-	
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
     
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
+    Joystick driveStick1 = new Joystick(1);
+    Joystick driveStick2 = new Joystick(2);
     
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
+    public enum Stick {
+        DRIVE_1, DRIVE_2;
+    }
     
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
+    public enum Axis {
+        X, Y, Z;
+    }
     
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+    public double getAxisValue(Stick stick, Axis axis) {
+        Joystick joystick = null;
+        switch (stick) {
+            case DRIVE_1:
+                joystick = driveStick1;
+                break;
+            case  DRIVE_2:
+                joystick = driveStick2;
+                break;
+            default:
+                return 0;
+        }
+        
+        switch (axis) {
+            case X:
+                return joystick.getX();
+            case Y:
+                return joystick.getY();
+            case Z:
+                return joystick.getZ();
+        }
+        
+        return 0;
+    }
 }
-
