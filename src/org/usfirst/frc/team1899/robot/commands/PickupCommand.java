1ce020e20cb4848ef9c50a113e979b7c0932215a
SunR <sunnyr2010@gmail.com>
2015-02-11 14:54:59 -0800

    Added Pickup system and changed Lifter

---
+++ src/org/usfirst/frc/team1899/robot/commands/PickupCommand.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team1899.robot.commands;
+
+import org.usfirst.frc.team1899.robot.OI.Axis;
+import org.usfirst.frc.team1899.robot.OI.Stick;
+import org.usfirst.frc.team1899.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class PickupCommand extends Command {
+
+    public PickupCommand() {
+        requires(Robot.pickupSubsystem);
+    }
+
+    protected void execute() {
+        Robot.pickupSubsystem.pickup(Robot.oi.getAxisValue(Stick.DRIVE_3, Axis.TRIGGERS));    
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void initialize() { }
+    protected void end() { }
+    protected void interrupted() {  }
+}
