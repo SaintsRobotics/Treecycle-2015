6951ac61dba5c0f1a3e225c9bee01c0d1653a07c
SunR <SunR@github.com>
2015-02-11 14:54:59 -0800

    Added Pickup system and changed Lifter

---
+++ src/org/usfirst/frc/team1899/robot/subsystems/PickupSubsystem.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team1899.robot.subsystems;
+
+import org.usfirst.frc.team1899.robot.RobotMap;
+import org.usfirst.frc.team1899.robot.commands.LifterCommand;
+import org.usfirst.frc.team1899.robot.commands.PickupCommand;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class PickupSubsystem extends Subsystem {
+    
+    SpeedController motor;
+    
+    public PickupSubsystem() {
+        motor = new Talon(RobotMap.WINCH);
+    }
+    
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new PickupCommand());
+    }
+    
+    public void pickup(double amount) {
+    	motor.set(amount);
+    }
+}
