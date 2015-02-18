87fa6935ce3c44ded24244596a34bded37287866
SunR <sunnyr2010@gmail.com>
2015-02-17 19:16:14 -0800

    Beginning of ramp.

--- src/org/usfirst/frc/team1899/robot/util/Ramp.java
+++ src/org/usfirst/frc/team1899/robot/util/Ramp.java
@@ -3,19 +3,22 @@ package org.usfirst.frc.team1899.robot.util;
 public class Ramp {
     
     double targetValue;
-    final double ratio;
+    double newValue;
     
-    public Ramp(double ratio) {
-        this.ratio = ratio;
+    public Ramp(double targetValue) {
+    	this.targetValue = targetValue;
     }
     
-    public double set(double newValue) {
-        targetValue = newValue;
-        return targetValue;
-        //return get();
+    public double rampValues(double oldValue, double targetValue) {
+    	if(Math.abs(oldValue - targetValue) > 0.5){
+    		if(oldValue < targetValue) newValue = oldValue + 0.5;
+    		else newValue = oldValue - 0.5;
+    	}
+    	return newValue;
     }
     
     public double get() {
-        return (Math.sin(Math.PI*(targetValue-1.0/2.0))+1)/2;
+    	
+        //return (Math.sin(Math.PI*(targetValue-1.0/2.0))+1)/2;
     }
 }
